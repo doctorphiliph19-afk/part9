c187abfba5eaae985d0a1a3b643660e29d704433
Student <student@sirafrica.org>
2026-09-07 10:39:59 +0300

    Complete Exercise 5: WebBmi

--- healthapp/bmiCalculator.ts
+++ healthapp/bmiCalculator.ts
@@ -1,32 +1,49 @@
-import { parseNumberArgument } from "./utils.js";
+interface BmiResult {
+  weight: number
+  height: number
+  bmi: string
+}
 
-const calculateBmi = (height: number, weight: number): string => {
-  const heightInMeters = height / 100;
-  const bmi = weight / (heightInMeters * heightInMeters);
+export const calculateBmi = (height: number, weight: number): string => {
+  const heightInMeters = height / 100
+  const bmi = weight / (heightInMeters * heightInMeters)
 
   if (bmi < 18.5) {
-    return 'Underweight';
+    return 'Underweight range'
   } else if (bmi < 25) {
-    return 'Normal range';
+    return 'Normal range'
   } else if (bmi < 30) {
-    return 'Overweight';
+    return 'Overweight range'
   } else {
-    return 'Obese';
+    return 'Obese range'
   }
-};
+}
 
-try {
-  const args = process.argv.slice(2);
-  
-  if (args.length < 2) {
-    throw new Error('Please provide height and weight as arguments');
+const parseArguments = (args: string[]): BmiResult => {
+  if (args.length < 4) {
+    throw new Error('malformatted parameters')
   }
-  
-  const height = parseNumberArgument(args[0]);
-  const weight = parseNumberArgument(args[1]);
-  
-  console.log(calculateBmi(height, weight));
-} catch (error) {
-  console.error('Error:', (error as Error).message);
-  process.exit(1);
+
+  const height = Number(args[2])
+  const weight = Number(args[3])
+
+  if (isNaN(height) || isNaN(weight)) {
+    throw new Error('malformatted parameters')
+  }
+
+  return {
+    height,
+    weight,
+    bmi: calculateBmi(height, weight)
+  }
+}
+
+if (process.argv[1] === import.meta.filename) {
+  try {
+    console.log(parseArguments(process.argv))
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.log(error.message)
+    }
+  }
 }
